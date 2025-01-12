728b61815aee61889c950cc565cc23afb742c6c6
Sindhu Satish <sindhusatish155@gmail.com>
2025-01-11 22:52:05 -0800

    remove incomplete information

--- src/assets/configs/projectConfig.js
+++ src/assets/configs/projectConfig.js
@@ -9,35 +9,35 @@ import portfolio from "../images/portfolio.png"
 import React from 'react'
 
 const projectConfig = [
-    {
-        id: "project-5",
-        title: "Retail Price Optimization",
-        links: [
-            {
-                name: "repo",
-                url: "https://github.com/sindhu-satish/Sklearn-genetic-opt",
-                icon: <AiFillGithub/>,
-            },
-            {
-                name: "fork",
-                url: "https://github.com/sindhu-satish/Sklearn-genetic-opt/fork",
-                icon: <BiGitRepoForked/>,
-            },
-            {
-                name: "subscription",
-                url: "https://github.com/sindhu-satish/Sklearn-genetic-opt/subscription",
-                icon: <AiFillEye/>,
-            },
-            {
-                name: "docs",
-                url: "https://sklearn-genetic-opt.readthedocs.io/en/stable/",
-                icon: <ImBook/>,
-            }
-        ],
-        image: sklearnGenetic,
-        description: "scikit-learn models hyperparameters tuning and feature selection, using evolutionary algorithms.",
-        target: "_blank"
-    }
+    // {
+    //     id: "project-5",
+    //     title: "Retail Price Optimization",
+    //     links: [
+    //         {
+    //             name: "repo",
+    //             url: "https://github.com/sindhu-satish/Sklearn-genetic-opt",
+    //             icon: <AiFillGithub/>,
+    //         },
+    //         {
+    //             name: "fork",
+    //             url: "https://github.com/sindhu-satish/Sklearn-genetic-opt/fork",
+    //             icon: <BiGitRepoForked/>,
+    //         },
+    //         {
+    //             name: "subscription",
+    //             url: "https://github.com/sindhu-satish/Sklearn-genetic-opt/subscription",
+    //             icon: <AiFillEye/>,
+    //         },
+    //         {
+    //             name: "docs",
+    //             url: "https://sklearn-genetic-opt.readthedocs.io/en/stable/",
+    //             icon: <ImBook/>,
+    //         }
+    //     ],
+    //     image: sklearnGenetic,
+    //     description: "scikit-learn models hyperparameters tuning and feature selection, using evolutionary algorithms.",
+    //     target: "_blank"
+    // }
 ]
 
 export default projectConfig
